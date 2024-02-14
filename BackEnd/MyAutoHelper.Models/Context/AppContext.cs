10b8568de8fde0e1187bee13bb539f995aaa3fdd
Polina Tselischeva <Polina.Tselischeva@intravision.ru>
2024-02-14 16:37:16 +0300

    Connect with database

---
+++ BackEnd/MyAutoHelper.Models/Context/AppContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyAutoHelper.Database.Models;
+
+namespace MyAutoHelper.Database.Context
+{
+    public class AppContext: DbContext
+    {
+        public AppContext()
+        {
+            //Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=MyAutoHelper;Username=USERNAME;Password=PASSWORD");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BackEnd/MyAutoHelper.Models/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace MyAutoHelper.Database.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
